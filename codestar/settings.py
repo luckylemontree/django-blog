6a4b00ee9ff144bece29c45f60756f1536f76b05
luckylemontree <mailwcq@gmail.com>
2026-06-17 10:53:46 +0100

    Add database configuration and environment setup in settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -9,6 +9,16 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+'''
+import the appropriate packages 
+(Note: You will use dj_database_url in a later step).
+ Now we connect the settings.py file to the env.py file
+ '''
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+
 
 from pathlib import Path
 
@@ -74,13 +84,16 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
