6299c10c696eca5d67be47ba3e108a5c67151805
luckylemontree <mailwcq@gmail.com>
2026-06-17 22:53:32 +0100

    Add updated_on field to Post model and create migration

--- blog/models.py
+++ blog/models.py
@@ -13,9 +13,10 @@ class Post(models.Model):
         related_name="blog_posts"
     )
     content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)   
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
