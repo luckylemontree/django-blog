8e4aa175c2aa79ff802dec462c24df02ef623a97
luckylemontree <mailwcq@gmail.com>
2026-06-21 20:56:13 +0100

    Set DEBUG to True for local development

--- codestar/settings.py
+++ codestar/settings.py
@@ -38,7 +38,7 @@ if os.path.isfile(os.path.join(BASE_DIR, "env.py")):
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY =os.environ.get("SECRET_KEY")        
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
