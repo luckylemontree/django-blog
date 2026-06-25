3bcbbe30f57dde29b75ec43b7d756386eaa08a5d
luckylemontree <mailwcq@gmail.com>
2026-06-25 08:30:00 +0100

    Add event detail view and update URLs

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from .models import Event
 
 # Create your views here.
 class PostList(generic.ListView):
@@ -31,4 +32,27 @@ def post_detail(request, slug):
         request,
         "blog/post_detail.html",
         {"post": post},
+    )
+
+class EventsList(generic.ListView):
+
+
+    model = Event
+    template_name = "index.html"
+    paginate_by = 12
+
+
+def event_detail(request, event_id):
+
+
+    # Database request
+
+    queryset = Event.objects.all()
+    event = get_object_or_404(queryset,event_id=event_id)
+
+
+    return render(
+        request,
+        "events/event_detail.html",
+        {"event": event},
     )
