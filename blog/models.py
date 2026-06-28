7ec9de3fa944f0a72245217fa68d748bf4496251
luckylemontree <mailwcq@gmail.com>
2026-06-28 14:02:54 +0100

    enable serving of image files

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
@@ -13,6 +14,9 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name="blog_posts"
         )
+    
+    featured_image = CloudinaryField('image', default='placeholder')
+    
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
